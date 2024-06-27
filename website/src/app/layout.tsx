2fce82273ef1b7be6ae8efbfb53a8f3423106199
nguyen2410 <ducnguyenbernardine@gmail.com>
2024-06-27 19:24:11 +0700

    .

---
+++ website/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "$Manifest",
+  description: "Greatest coin ever",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} w-full h-screen bg-white`}>{children}</body>
+    </html>
+  );
+}
